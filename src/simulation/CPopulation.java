882382e6277cd445840ad025ae11df89c046387e
shunichiYasuda <34476078+shunichiYasuda@users.noreply.github.com>
2022-02-17 18:28:57 +0900

    edit 18:28

--- src/simulation/CPopulation.java
+++ src/simulation/CPopulation.java
@@ -1,5 +1,10 @@
 package simulation;
 
 public class CPopulation {
-
+	int popSize;
+	CIndividual[] member;
+	// 統計値:平均、分散、最大値、最小値、標準偏差
+	double mAve, mDev, mMax, mMin, mSigma;
+	// 最大値個体の番号
+	int mMaxID;
 }
